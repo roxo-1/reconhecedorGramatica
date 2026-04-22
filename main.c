874d92f662bd23b7b8b911b6f1bd6e53c9cc98ab
Carolina Lee <111212023+roxo-1@users.noreply.github.com>
2026-04-22 08:43:13 -0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void E(){
+    char *entrada = "+*aba";
+	switch(*entrada){// testa o in´ıcio da palavra
+		case '+':
+			entrada++;
+			E();
+			break;
+		case '*':
+			entrada++;
+			E();
+			break;
+		case 'a':
+			entrada++; // consome a (deriva a)
+			(); //chama o terminal E()
+			break;
+		case 'b':
+			entrada++; // consome b (deriva b)
+			(); // chama o terminal E()
+			break;
+		default:
+			printf("\nerro.");
+			exit(1); // finaliza o programa
+	}
+}
+
+int main(){
+    char *entrada = "+*aba";
+	printf("analisando a entrada %s\n",entrada);
+	S(); // s´ımbolo inicial a gram´atica
+	// se chegou ao final da palavra, sem erro,
+	// a palavra pertence `a linguagem gerada pela gram´atica.
+	if( *entrada == '\x0')// fim de entrada
+		printf("\npalavra pertence a.\n");
+	else
+		printf("\nerro.");
+	printf("\nfim de programa.\n");
+	return 0;
+}
+
